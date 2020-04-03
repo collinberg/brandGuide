103ef2d7ed421f25203d32ede214ebcbe9bfd8f5
Collin B <berg.collin@gmail.com>
2020-04-03 16:41:35 -0400

    Init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,43 @@
+// Example `tailwind.config.js` file
+
 module.exports = {
+  important: true,
   theme: {
-    extend: {},
+    fontFamily: {
+      'sans': [
+        'benton-sans',
+        'system-ui',
+        'BlinkMacSystemFont',
+        '-apple-system',
+        'Helvetica Neue',
+        'sans-serif',
+      ],
+      display: ['Gilroy', 'sans-serif'],
+      body: ['Graphik', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        primary: {
+          lighter: '#ffad81',
+          default: '#ec6433',
+          dark: '#C26033',
+        },
+        gray: {
+          '300': '#e6e7e9',
+          '500': '#8E8F97',
+          '700': '#4a5568',
+          '800': '#424242',
+          '900': '#212121',
+
+        }
+      },
+      margin: {
+        '96': '24rem',
+        '128': '32rem',
+      },
+    }
   },
-  variants: {},
-  plugins: [],
+  variants: {
+    opacity: ['responsive', 'hover']
+  }
 }
